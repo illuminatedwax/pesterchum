e0bfd6418413668afb5d503fd61b61b9d22b3b8c
Stephen Dranger <dranger@choicescounselingservices.com>
2011-02-18 20:01:03 -0600

    derp

---
+++ themes/trollian2.5/style.js
@@ -0,0 +1,94 @@
+{"inherits": "pesterchum2.5",
+ "main":
+ {"background-image": "$path/tnbg2.png",
+  "size": [300,620],
+  "icon": "$path/trayicon.png",
+  "newmsgicon": "$path/trayicon2.png",
+  "windowtitle": "TROLLIAN",
+  "menu" : { "style": "font-family: 'Arial'; font: bold; font-size: 14px; background-color: #e5000f;border:2px solid #c20f00" }
+  "close": { "image": "$path/x.gif",
+             "loc": [280, 2]},
+  "minimize": { "image": "$path/m.gif",
+                "loc": [260, 8]},
+  "chums": { "style": "border:2px solid #ffa4a4; background-color: black;color: white;font: bold;font-size:14px;font-family: 'Arial';selection-background-color:#646464; " },
+  "mychumhandle": { "label": 
+                    { "text": "TROLLTAG:",
+                      "style": "color: black ;font:bold; font-family: 'Arial';" 
+                    },
+					"handle": { "style": "background-color: black; padding: 3px; padding-left: 25px; color:white; font-family:'Arial'; font:bold; text-align:left; border:2px solid #ffa4a4;" }
+                  },
+  "addchum":  { "style": "background: #ffa4a4; border:2px solid #780000; font: bold; color: black; font-family:'Arial';"
+              },
+  "pester": { "style": "background: #ffa4a4; border:2px solid #780000; font: bold; color: black; font-family:'Arial';"
+            },
+  "block": { "style": "background: #ffa4a4; border:2px solid #780000; font: bold; color: black; font-family:'Arial';"
+           },
+  "moodlabel": { "style": "font:bold;font-family:'Arial';color:black;"
+			   },
+  "moods": [
+      { "style": "text-align:left; background:#ffa4a4;border:2px solid #780000;color: black; font-family:'Arial'; font:bold; padding-left:3px;", 
+		"selected": "text-align:left; background:white; border:2px solid #780000; color: black; font-family:'Arial'; font:bold; padding-left:3px;",
+	    "text": "ECSTATIC",
+		"icon": "$path/estatic.gif",
+		"mood": 7
+	  },
+
+      { "style": "text-align:left; background:#ffa4a4;border:2px solid #780000;color: black; font-family:'Arial'; font:bold; padding-left:3px;", 
+		"selected": "text-align:left; background:white; border:2px solid #780000; color: black; font-family:'Arial'; font:bold; padding-left:3px;",
+	    "text": "RELAXED",
+		"icon": "$path/relaxed.gif",
+		"mood": 8
+	  },
+
+      { "style": "text-align:left; background:#ffa4a4;border:2px solid #780000;color: black; font-family:'Arial'; font:bold; padding-left:3px;", 
+		"selected": "text-align:left; background:white; border:2px solid #780000; color: black; font-family:'Arial'; font:bold; padding-left:3px;",
+	    "text": "DISCONTENT",
+		"icon": "$path/discontent.gif",
+		"mood": 9
+	  },
+
+      { "style": "text-align:left; background:#ffa4a4;border:2px solid #780000;color: black; font-family:'Arial'; font:bold; padding-left:3px;", 
+		"selected": "text-align:left; background:white; border:2px solid #780000; color: black; font-family:'Arial'; font:bold; padding-left:3px;",
+	    "text": "DEVIOUS",
+		"icon": "$path/devious.gif",
+		"mood": 10
+	  },
+
+      { "style": "text-align:left; background:#ffa4a4;border:2px solid #780000;color: black; font-family:'Arial'; font:bold; padding-left:3px;", 
+		"selected": "text-align:left; background:white; border:2px solid #780000; color: black; font-family:'Arial'; font:bold; padding-left:3px;",
+	    "text": "SLEEK",
+		"icon": "$path/sleek.gif",
+		"mood": 11
+	  },
+
+      { "style": "text-align:left; background:#ffa4a4;border:2px solid #780000;color: black; font-family:'Arial'; font:bold; padding-left:3px;", 
+		"selected": "text-align:left; background:white; border:2px solid #780000; color: black; font-family:'Arial'; font:bold; padding-left:3px;",
+	    "text": "DETESTFUL",
+		"icon": "$path/detestful.gif",
+		"mood": 12
+	  },
+
+      { "style": "text-align:center; border:2px solid #780000; background:black;color: white; font-family:'Arial'; font:bold;padding-left:3px;", 
+		"selected": "text-align:left; background:white; border:2px solid #780000; padding: 5px;color: black; font-family:'Arial'; font:bold;padding-left:3px;",
+		"loc": [15, 569],
+		"size": [270, 30],
+		"text": "ABSCOND",
+		"icon": "$path/offline.gif",
+		"mood": 2
+	  }
+  ]
+ },
+ "convo": {
+     "style": "background-color: #e5000f;border:2px solid #780000; font-family: 'Arial';",
+     "tabstyle": "background-color: #e5000f; font-family: 'Arial'",
+     "chumlabel": { "style": "margin-bottom: 21px;background: #ffa4a4; color: black; border:0px; font-size: 14px;",
+					"text" : ":: trolling: $handle ::" }
+
+ },
+ "memos":
+ { "size": [600,425] }
+}
+
+
+
+  
