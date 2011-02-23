e495ca7d24f73b34e9108cab3dc244f6b6035754
Stephen Dranger <dranger@gmail.com>
2011-02-23 05:11:56 -0600

    derp

--- menus.py
+++ menus.py
@@ -731,6 +731,6 @@ class LoadingScreen(QtGui.QDialog):
 class AboutPesterchum(QtGui.QMessageBox):
     def __init__(self, parent=None):
         QtGui.QMessageBox.__init__(self, parent)
-        self.setText("P3ST3RCHUM V. 3.14 alpha 6")
+        self.setText("P3ST3RCHUM V. 3.14 alpha 7")
         self.setInformativeText("Programming by illuminatedwax (ghostDunk), art by Grimlive (aquaMarinist)")
         self.mainwindow = parent
