35845db8e3c6d881a006ddc9d97ee5b2e62f8319
Kiooeht <kiooeht@gmail.com>
2011-06-19 05:32:33 -0700

    Bump dev up to Beta 5

--- version.py
+++ version.py
@@ -10,7 +10,7 @@ _pcStatus = "B" # A  = alpha
                 # B  = beta
                 # RC = release candidate
                 # None = public release
-_pcRevision = "4"
+_pcRevision = "5"
 _pcVersion = ""
 
 def pcVerCalc():
