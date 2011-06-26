f7618bccecc622203f09aed834ce7e82eadb3366
illuminatedwax <dranger@gmail.com>
2011-06-26 17:16:30 -0500

    derp, flush()

--- pesterchum_debug.py
+++ pesterchum_debug.py
@@ -4,6 +4,7 @@ import datetime
 f = open("debug.log", 'a')
 d = datetime.datetime.now()
 f.write("=== PESTERCHUM DEBUG %s ===\n" % d.strftime("%m-%d-%y %H-%M"))
+f.flush()
 p = subprocess.Popen("pesterchum.exe", stdout=f, stderr=subprocess.STDOUT)
 p.wait()
 f.close()
